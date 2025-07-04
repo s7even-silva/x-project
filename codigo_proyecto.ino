fe9c25cf4c65377d8e30de32984d073d385ee90d
Bryam Silva <bryamsilvan@gmail.com>
2025-07-04 00:45:05 -0500

    Comentarios y detalles del código

--- codigo_proyecto.ino
+++ codigo_proyecto.ino
@@ -32,8 +32,9 @@ int       buttonState    = LOW;
 int       lastButtonState= LOW;
 
 void setup() {
-  Serial.begin(9600);
+  Serial.begin(115200);
 
+  // Muestra el logo al encender el equipo
   delay(250);
   display.begin(I2C_ADDRESS, true);
   display.clearDisplay();
@@ -46,6 +47,8 @@ void setup() {
     display.clearDisplay();
   }
 
+  // Inicializando el sensor de temperatura, el sensor TDS, y el botón.
+  // Además limpiando el contenido de analogBuffer por seguridad.
   dsSensor.begin();
   for (int i = 0; i < numSamples; i++) analogBuffer[i] = 0;
   pinMode(buttonPin, INPUT);
@@ -53,6 +56,9 @@ void setup() {
 }
 
 void loop() {
+
+  // Espera a que se presione el botón para hacer
+  // una llamada a la función hacerMediciones()
   buttonState = digitalRead(buttonPin);
   if (buttonState == HIGH && lastButtonState == LOW) {
     hacerMediciones();
@@ -62,10 +68,13 @@ void loop() {
 }
 
 void hacerMediciones() {
+
+  // Declarando constantes para el uso posterior
   const int totalMed = 20;
   float sumaTdsComp = 0.0;
   float sumaTemp    = 0.0;
 
+  // Muestra en la pantalla la palabra "Midiendo"
   display.clearDisplay();
   display.setTextSize(2);
   display.setTextColor(SH110X_WHITE);
@@ -73,6 +82,9 @@ void hacerMediciones() {
   display.print("Midiendo");
   int barY = 32;
   int barH = 10;
+
+  // Muestra una barra de progreso que se actualiza
+  // según la cantidad de mediciones realizadas.
   display.drawRect(0, barY, SCREEN_WIDTH, barH, SH110X_WHITE);
   display.display();
 
@@ -80,11 +92,13 @@ void hacerMediciones() {
   for (int i = 0; i < numSamples; i++) analogBuffer[i] = 0;
 
   for (int i = 1; i <= totalMed; i++) {
+    // Medición de temperatura
     dsSensor.requestTemperatures();
     float tempC = dsSensor.getTempCByIndex(0);
     if (tempC == DEVICE_DISCONNECTED_C) tempC = 25.0;
     sumaTemp += tempC;
 
+    // Medición de TDS
     analogBuffer[bufIndex++] = analogRead(tdsPin);
     if (bufIndex >= numSamples) bufIndex = 0;
     
@@ -94,12 +108,14 @@ void hacerMediciones() {
     float voltage = avgAdc * VREF / ADC_RES;
     float tdsRaw  = 133.42 * pow(voltage, 3)
                    - 255.86 * pow(voltage, 2)
-                   + 857.39 * voltage;
+                   + 857.39 * voltage; // Fórmula para calcular TDS
 
-    float tdsComp = tdsRaw / (1.0 + 0.02 * (tempC - 25.0));
+    float tdsComp = tdsRaw / (1.0 + 0.02 * (tempC - 25.0)); // Compensación por temperatura
+    tdsComp = 1.6442 * tdsComp; // Compensación por mediciones experimentales
 
     sumaTdsComp += tdsComp;
 
+    // Actualización de la barra de progreso
     int barWidth = map(i, 0, totalMed, 0, SCREEN_WIDTH);
     display.fillRect(1, barY+1, barWidth-2, barH-2, SH110X_WHITE);
     display.display();
@@ -107,9 +123,12 @@ void hacerMediciones() {
     delay(50);
   }
 
+  // Cálculo de los promedios
   float promedioTds = sumaTdsComp / totalMed;
   float promedioT   = sumaTemp    / totalMed;
 
+  // Mostrado de los resultados en pantalla
+  // y en el monitor serial
   display.clearDisplay();
   display.setTextSize(2);
   display.setTextColor(SH110X_WHITE);
